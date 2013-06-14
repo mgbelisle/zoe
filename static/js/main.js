e5303505e83b293c31e01c970aa33440d91eab73
Matt Belisle <mgbelisle@gmail.com>
2013-06-14 13:30:23 -0600

    Combining bootstrap and angular

--- static/js/main.js
+++ static/js/main.js
@@ -2,24 +2,10 @@
 
 /* App Module */
 
-angular.module('phonecat', []).
-  config(['$routeProvider', function($routeProvider) {
-  $routeProvider.
-      when('/phones', {templateUrl: '/static/html/phone-list.html',   controller: PhoneListCtrl}).
-      when('/phones/:phoneId', {templateUrl: '/static/html/phone-detail.html', controller: PhoneDetailCtrl}).
-      otherwise({redirectTo: '/phones'});
-}]);
+angular.module('zoe', []).
+    config(['$routeProvider', function($routeProvider) {
+	$routeProvider.
+	    when('/home', {templateUrl: '/static/html/home.html'}).
+	    otherwise({redirectTo: '/home'});
+    }]);
 
-/* Controllers */
-
-function PhoneListCtrl($scope, $http) {
-  $http.get('/static/js/phones.json').success(function(data) {
-    $scope.phones = data;
-  });
-
-  $scope.orderProp = 'age';
-}
-
-function PhoneDetailCtrl($scope, $routeParams) {
-  $scope.phoneId = $routeParams.phoneId;
-}
