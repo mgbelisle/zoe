c47e3099ec3cc256588571bdecf07171dadb0ad4
Matt Belisle <mgbelisle@gmail.com>
2013-06-14 12:55:40 -0600

    Setting up phonecat app

--- static/js/main.js
+++ static/js/main.js
@@ -1,14 +1,25 @@
-// Sets up the navigation tabs
-$(function () {
-    if (window.location.hash) {
-	var href = window.location.hash;
-    } else {
-	var href = '#';
-    }
-    $('ul.nav a[href="' + href + '"]').tab('show');
-})
+'use strict';
 
-$('ul.nav a').click(function (e) {
-    $(this).tab('show');
-    return true;
-})
+/* App Module */
+
+angular.module('phonecat', []).
+  config(['$routeProvider', function($routeProvider) {
+  $routeProvider.
+      when('/phones', {templateUrl: '/static/html/phone-list.html',   controller: PhoneListCtrl}).
+      when('/phones/:phoneId', {templateUrl: '/static/html/phone-detail.html', controller: PhoneDetailCtrl}).
+      otherwise({redirectTo: '/phones'});
+}]);
+
+/* Controllers */
+
+function PhoneListCtrl($scope, $http) {
+  $http.get('/static/js/phones.json').success(function(data) {
+    $scope.phones = data;
+  });
+
+  $scope.orderProp = 'age';
+}
+
+function PhoneDetailCtrl($scope, $routeParams) {
+  $scope.phoneId = $routeParams.phoneId;
+}
