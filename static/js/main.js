af68f1bb6596e494f8697ca543264c6247138add
Matt Belisle <mgbelisle@gmail.com>
2013-06-14 13:45:52 -0600

    Getting templates working a little

--- static/js/main.js
+++ static/js/main.js
@@ -6,6 +6,10 @@ angular.module('zoe', []).
     config(['$routeProvider', function($routeProvider) {
 	$routeProvider.
 	    when('/home', {templateUrl: '/static/html/home.html'}).
+	    when('/services', {templateUrl: '/static/html/services.html'}).
+	    when('/about', {templateUrl: '/static/html/about.html'}).
+	    when('/newsletter', {templateUrl: '/static/html/newsletter.html'}).
+	    when('/contact', {templateUrl: '/static/html/contact.html'}).
 	    otherwise({redirectTo: '/home'});
     }]);
 
