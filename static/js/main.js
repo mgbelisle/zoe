5cc4866c17635329dba2647fd3fbdeac87ee5c46
Matt Belisle <mgbelisle@gmail.com>
2013-06-16 13:03:35 -0600

    Getting isActiveTab() working

--- static/js/main.js
+++ static/js/main.js
@@ -2,14 +2,20 @@
 
 /* App Module */
 
-angular.module('zoe', []).
-    config(['$routeProvider', function($routeProvider) {
-	$routeProvider.
-	    when('/home', {templateUrl: '/static/html/home.html'}).
-	    when('/services', {templateUrl: '/static/html/services.html'}).
-	    when('/about', {templateUrl: '/static/html/about.html'}).
-	    when('/newsletter', {templateUrl: '/static/html/newsletter.html'}).
-	    when('/contact', {templateUrl: '/static/html/contact.html'}).
-	    otherwise({redirectTo: '/home'});
-    }]);
+var zoeApp = angular.module('zoe', []);
+    
+zoeApp.config(['$routeProvider', function($routeProvider) {
+    $routeProvider.
+	when('/home', {templateUrl: '/static/html/home.html', controller: 'MainCtrl', tab: 'home'}).
+	when('/services', {templateUrl: '/static/html/services.html', controller: 'MainCtrl', tab: 'services'}).
+	when('/about', {templateUrl: '/static/html/about.html', controller: 'MainCtrl', tab: 'about'}).
+	when('/newsletter', {templateUrl: '/static/html/newsletter.html', controller: 'MainCtrl', tab: 'newsletter'}).
+	when('/contact', {templateUrl: '/static/html/contact.html', controller: 'MainCtrl', tab: 'contact'}).
+	otherwise({redirectTo: '/home'});
+}]);
 
+zoeApp.controller('MainCtrl', ['$rootScope', '$route', function($rootScope, $route) {
+    $rootScope.isActiveTab = function(tab) {
+	return tab == $route.current.tab;
+    }
+}]);
