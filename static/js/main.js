4cdd4d9d911249694ad44a945d7c4c43b72a0f83
Matt Belisle <mgbelisle@gmail.com>
2013-08-14 13:24:34 -0600

    Cleaning up navs

--- static/js/main.js
+++ static/js/main.js
@@ -16,14 +16,14 @@ zoeApp.config(['$routeProvider', function($routeProvider) {
 	when('/sti', {templateUrl: '/static/html/sti/home.html'}).
 	when('/sti/faq', {templateUrl: '/static/html/sti/faq.html'}).
 	when('/sti/symptoms', {templateUrl: '/static/html/sti/symptoms.html'}).
-	when('/help', {templateUrl: '/static/html/help.html'}).
-	when('/about', {templateUrl: '/static/html/about/home.html'}).
-	when('/about/services', {templateUrl: '/static/html/about/services.html'}).
-	when('/about/staff', {templateUrl: '/static/html/about/staff.html'}).
-	when('/about/board', {templateUrl: '/static/html/about/board.html'}).
-	when('/about/mission', {templateUrl: '/static/html/about/mission.html'}).
-	when('/help', {templateUrl: '/static/html/help.html'}).
-	when('/news', {templateUrl: '/static/html/news.html'}).
+	when('/services', {templateUrl: '/static/html/services.html'}).
+	// TODO: Add later
+	// when('/help', {templateUrl: '/static/html/help.html'}).
+	// when('/about', {templateUrl: '/static/html/about/home.html'}).
+	// when('/about/staff', {templateUrl: '/static/html/about/staff.html'}).
+	// when('/about/board', {templateUrl: '/static/html/about/board.html'}).
+	// when('/about/mission', {templateUrl: '/static/html/about/mission.html'}).
+	// when('/news', {templateUrl: '/static/html/news.html'}).
 	when('/contact', {templateUrl: '/static/html/contact.html'}).
 	otherwise({redirectTo: '/'});
 }]);
