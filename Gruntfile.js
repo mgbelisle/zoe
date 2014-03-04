1c35e1e4930d7bcbd599653d8e22ce6ac6799c77
Matt Belisle <mgbelisle@gmail.com>
2014-03-04 08:06:38 -0700

    Maybe making static/src dir?

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,16 +2,17 @@ module.exports = function(grunt) {
     grunt.initConfig({
         pkg: grunt.file.readJSON('package.json'),
         jshint: {
-            files: ['Gruntfile.js', 'static/js/src/**'],
+            files: ['Gruntfile.js', 'static/js/**'],
         },
         requirejs: {
             compile: {
                 options: {
-                    mainConfigFile: 'js/src/main.js',
-                    baseUrl: "static",
-                    out: "js/build.js",
-                    cssIn: "css/src/index.css",
-                    cssOut: "css/build.css",
+                    mainConfigFile: 'js/main.js',
+                    appDir: './static/',
+                    baseUrl: './',
+                    name: './static/js/main',
+                    dir: './static/build',
+                    // cssIn: "./static/css/src/main.css",
                     optimize: "uglify2"
                 }
             }
