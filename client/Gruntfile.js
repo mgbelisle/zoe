afe1d5712cd03eb98f96d0f744a6733b8978c9cc
Matthew Belisle <matthew.belisle@webfilings.com>
2014-03-04 10:05:18 -0700

    Moving to client and api folders

---
+++ client/Gruntfile.js
@@ -0,0 +1,26 @@
+module.exports = function(grunt) {
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+        jshint: {
+            files: ['Gruntfile.js', 'src/js/**'],
+        },
+        requirejs: {
+            compile: {
+                options: {
+                    mainConfigFile: 'js/main.js',
+                    appDir: './static/',
+                    baseUrl: './',
+                    name: './static/js/main',
+                    dir: './static/build',
+                    // cssIn: "./static/css/src/main.css",
+                    optimize: "uglify2"
+                }
+            }
+        }
+    });
+
+    grunt.loadNpmTasks('grunt-contrib-jshint');
+    grunt.loadNpmTasks('grunt-contrib-requirejs');
+
+    grunt.registerTask('default', ['jshint', 'requirejs']);
+};
