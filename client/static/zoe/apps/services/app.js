5f44e5628fef27d78eddc286da47fbb301e28702
Matthew Belisle <matthew.belisle@webfilings.com>
2014-04-24 21:57:27 -0500

    Need to think more about urls

--- client/static/zoe/apps/services/app.js
+++ client/static/zoe/apps/services/app.js
@@ -1,38 +1,26 @@
-// var angular = require('angular'); // TODO
-// var angularRoute = require('angular-route'); // TODO
-var angular = require('angular-tmp');
-var bootstrap = require('bootstrap-tmp');
+var angular = require('angular');
 
 var NAME = 'zoe.services';
 
 var app = angular.module(NAME, [
-    // 'ngRoute', // TODO
     require('../common/app')
 ]);
 
-app.config(['$routeProvider', function($routeProvider) {
-    $routeProvider.
-        when('/services', {template: 'This is services'});
-}]);
-
 app.run(['headerService', function(headerService) {
-    headerService.setNavbarItems(
-        '/services(/.*)?',
+    headerService.setNavItems(
+        {kingdom: 'services'},
         [
             {
                 href: '/services/pregnancy',
-                matchRoute: '/services(/.*)?',
+                params: {kingdom: 'services',
+                         phylum: 'pregnancy'},
                 text: 'Pregnancy'
             },
             {
                 href: '/services/sti',
-                matchRoute: '/services/sti(/.*)?',
+                params: {kingdom: 'services',
+                         phylum: 'sti'},
                 text: 'STD Testing'
-            },
-            {
-                href: '/contact',
-                matchRoute: '/contact(/.*)?',
-                text: 'Contact Us'
             }
         ]);
 }]);
