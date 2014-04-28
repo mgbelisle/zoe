83d1f25755ca5dd810553c08c74b8885ff2802db
Matthew Belisle <matthew.belisle@webfilings.com>
2014-04-28 14:24:42 -0400

    Putting ng-view back

--- client/static/zoe/route.js
+++ client/static/zoe/route.js
@@ -1,7 +1,10 @@
+var fs = require('fs');
+
 var config = function($locationProvider, $routeProvider) {
     $locationProvider.html5Mode(true);
     $routeProvider.
-        when('/:kingdom?/:phylum?/:class?/:order?/:family?/:genus?/:species?', {}).
+        when('/:kingdom?/:phylum?/:class?/:order?/:family?/:genus?/:species?',
+             {template: fs.readFileSync(__dirname + '/body.html', 'utf8')}).
         otherwise({redirectTo: '/'});
 };
 
