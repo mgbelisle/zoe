890214edbb50feaf98c952d8fcdd5f8051ef678b
Matthew Belisle <matthew.belisle@webfilings.com>
2014-05-17 00:30:24 -0600

    /services/pregnancy/ultrasounds done

--- client/static/zoe/services/app.js
+++ client/static/zoe/services/app.js
@@ -17,6 +17,7 @@ app.config([
             when('/services/pregnancy', {template: fs.readFileSync(__dirname + '/html/pregnancy/home.html', 'utf8')}).
             when('/services/pregnancy/symptoms', {template: fs.readFileSync(__dirname + '/html/pregnancy/symptoms.html', 'utf8')}).
             when('/services/pregnancy/testing', {template: fs.readFileSync(__dirname + '/html/pregnancy/testing.html', 'utf8')}).
+            when('/services/pregnancy/ultrasounds', {template: fs.readFileSync(__dirname + '/html/pregnancy/ultrasounds.html', 'utf8')}).
             when('/services/sti', {template: fs.readFileSync(__dirname + '/html/sti/home.html', 'utf8')}).
             when('/services/contact', {template: fs.readFileSync(__dirname + '/html/contact.html', 'utf8')});
     }
