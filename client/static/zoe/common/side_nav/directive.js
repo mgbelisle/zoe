cad3f5fd62e4bc64572c06af29ead06d4dd2b42b
Matt Belisle <mgbelisle@gmail.com>
2014-05-17 20:47:48 -0600

    Fixing directives

--- client/static/zoe/common/side_nav/directive.js
+++ client/static/zoe/common/side_nav/directive.js
@@ -1,14 +1,14 @@
 var fs = require('fs');
 
-var directive = function(urlService) {
-    return {
-        restrict: 'E',
-        link: function(scope, element, attrs) {
-            scope.isActive = urlService.isActive;
-        },
-        template: fs.readFileSync(__dirname + '/template.html', 'utf8')
-    };
-};
-directive.$injects = ['urlService'];
-
-module.exports = directive;
+module.exports = [
+    'urlService',
+    function(urlService) {
+        return {
+            restrict: 'E',
+            link: function(scope, element, attrs) {
+                scope.isActive = urlService.isActive;
+            },
+            template: fs.readFileSync(__dirname + '/template.html', 'utf8')
+        };
+    }
+];
