219f2b7da5fca79c28ad8d44dc2a0ebc16b59e67
Matthew Belisle <matthew.belisle@webfilings.com>
2014-07-12 11:52:58 -0600

    Trying staff links, not so cool

--- client/static/zoe/support/staff/controller.js
+++ client/static/zoe/support/staff/controller.js
@@ -1,12 +1,28 @@
 var fs = require('fs');
 
-var Controller = function($scope) {
+var Controller = function(
+    $location,
+    $routeParams,
+    $scope
+) {
     $scope.members = require('./members.js');
-    $scope.member = $scope.members[0];
-    $scope.setMember = function(member) {
-        $scope.member = member;
-    };
+    var membersMap = {};
+    $.each($scope.members, function(i, member) {
+        membersMap[member.key] = member;
+    });
+    if ($routeParams.key) {
+        $scope.member = membersMap[$routeParams.key];
+        if (!$scope.member) {
+            $location.path('/support/about/staff');
+        }
+    } else {
+        $scope.member = $scope.members[0];
+    }
 };
-Controller.$inject = ['$scope'];
+Controller.$inject = [
+    '$location',
+    '$routeParams',
+    '$scope'
+];
 
 module.exports = Controller;
