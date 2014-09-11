6cb047171be839d8e12a495519ce8c010d161a85
Matthew Belisle <matthew.belisle@webfilings.com>
2014-09-11 16:46:20 -0600

    Fixing zoe-href

--- client/src/common/href_directive.js
+++ client/src/common/href_directive.js
@@ -1,12 +1,16 @@
-var directive = function () {
+$ = require('jquery');
+
+var directive = function ($window) {
     return function(scope, element, attrs) {
-        element.click(function() {
-            window.open(element.attr('href'),
-                        element.attr('href'),
-                        'width=768,height=612,scrollbars=1');
+        $(element).click(function() {
+            $window.open(element.attr('href'),
+                         element.attr('href'),
+                         'width=768,height=612,scrollbars=1');
             return false;
         });
     };
 };
+
+directive.$inject = ['$window'];
 
 module.exports = directive;
