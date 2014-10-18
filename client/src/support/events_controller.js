00943338f3fb3bc91318a44db97ac7cdcbce48fd
Matthew Belisle <matthew.belisle@webfilings.com>
2014-10-18 17:54:21 -0400

    Adding event location

--- client/src/support/events_controller.js
+++ client/src/support/events_controller.js
@@ -1,3 +1,5 @@
+var $ = require('jquery');
+
 var Controller = function(
     $http,
     $scope
@@ -7,6 +9,17 @@ var Controller = function(
         success(function(data) {
             $scope.events = data;
         });
+    $scope.mapURL = function(event) {
+        var params = {
+            q: event.address,
+            z: '16',
+        };
+        if (event.location) {
+            // Lat/long
+            params.sll = String(event.location[0]) + ',' + String(event.location[1]);
+        }
+        return 'https://maps.google.com/maps?' + $.param(params);
+    };
 };
 Controller.$inject = [
     '$http',
